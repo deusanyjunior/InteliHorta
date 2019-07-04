9f61f61a8bf6e8a4b1789162ecee2e2088686c48
Antonio Deusany de Carvalho Junior <deusanyjunior@gmail.com>
2019-07-04 11:48:37 -0300

    Change the post process

--- IoTreesSensingTS.ino
+++ IoTreesSensingTS.ino
@@ -1,51 +1,43 @@
-//#include <ESP8266HTTPClient.h>
+#include "ThingSpeak.h"
 #include <ESP8266WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <dht.h>
+#include <dht.h> // https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTstable
 #include "keys.h"
 
 // WiFi
 WiFiClient client;
 
-// ThingSpeak
-const char* server = "api.thingspeak.com";
-
-// Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS D4
- 
-// Setup a oneWire instance to communicate with any OneWire devices 
-// (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
- 
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
-
-// DHT22 settings
+// DHT22
 dht DHT;
 #define DHT22_PIN D5
 
 // SOIL MOISTURE
 #define SM_PIN A0
 
+// DS18B20
+#define ONE_WIRE_BUS D4 // Data wire is plugged into pin 2 on the Arduino
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
+
 void setup(void)
 {
-  // Start serial port
   Serial.begin(9600);
   
-  // Start WiFi
-  wifiConnection();
-  
-  // Start up sensors' library
-  sensors.begin();
+  // WiFi
+  wifiConnection(); // Setup Wifi
+    
+  // DHT22
+  pinMode(DHT22_PIN, INPUT); // Set DHT22 as input
 
-  // Setup DS18B20  
-  pinMode(ONE_WIRE_BUS, INPUT);
+  // DS18B20
+  sensors.begin(); // Start up sensors' library 
+  pinMode(ONE_WIRE_BUS, INPUT); // Set DS18B20 as input
+
+  // ThingSpeak
+  ThingSpeak.begin(client); // Start the client
   
-  // Setup DHT22
-  pinMode(DHT22_PIN, INPUT);
-  
-  delay(1000);
+  delay(1000); // Wait the setup to finish
 }
 
 void loop(void)
@@ -53,54 +45,90 @@ void loop(void)
   // Verify connection
   while (WiFi.status() != WL_CONNECTED) {
     wifiConnection();
-    delay(500);
-    Serial.println(".");
   }
-
-  // Get Data
 
   // DHT22
   requestDHT22Data();
   float dht22T = DHT.temperature;
   float dht22H = DHT.humidity;
   
+  // Soil Moisture
+  float soilMoisture = analogRead(SM_PIN);
+  float smMapped = map(soilMoisture,750,250,0,100); // Map values from sensor
+
   // DS18B20
   requestDS18B20Data();
   float ds18b20T = sensors.getTempCByIndex(0);
 
-  // Soil Moisture
-  float soilMoisture = analogRead(SM_PIN);
-  float smMapped = map(soilMoisture,750,250,0,100);
+  // Set the fields with the values
+  ThingSpeak.setField(1, dht22T);
+  ThingSpeak.setField(2, dht22H);
+  ThingSpeak.setField(3, ds18b20T);
+  ThingSpeak.setField(4, smMapped);
 
-  // Send Data
-  if (client.connect(server,80)) {
-    String postStr = apiKey;
-           postStr +="&amp;field1=";
-           postStr += String(dht22T);
-           postStr +="&amp;field2=";
-           postStr += String(dht22H);
-           postStr +="&amp;field3=";
-           postStr += String(ds18b20T);
-           postStr +="&amp;field4=";
-           postStr += String(smMapped);
-           postStr += "\r\n\r\n";
- 
-     client.print("POST /update HTTP/1.1\n");
-     client.print("Host: api.thingspeak.com\n");
-     client.print("Connection: close\n");
-     client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
-     client.print("Content-Type: application/x-www-form-urlencoded\n");
-     client.print("Content-Length: ");
-     client.print(postStr.length());
-     client.print("\n\n");
-     client.print(postStr);
-
-     Serial.print("Post message: ");
-     Serial.println(postStr);
+  // Send data to ThingSpeak
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
   }
-  client.stop();  
-  
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+
   delay(30000);
+}
+
+// Method to (re)connect to WiFi network
+void wifiConnection() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Function to request DHT22 status and values
+void requestDHT22Data() {
+  uint32_t start = micros();
+  int chk = DHT.read22(DHT22_PIN);
+  uint32_t stop = micros();
+
+  Serial.print("DHT22,\t");
+  
+//  stat.total++;
+  switch (chk)
+  {
+  case DHTLIB_OK:
+//      stat.ok++;
+      Serial.print("OK,\t");
+      break;
+  case DHTLIB_ERROR_CHECKSUM:
+//      stat.crc_error++;
+      Serial.print("Checksum error,\t");
+      break;
+  case DHTLIB_ERROR_TIMEOUT:
+//      stat.time_out++;
+      Serial.print("Time out error,\t");
+      break;
+  default:
+//      stat.unknown++;
+      Serial.print("Unknown error,\t");
+      break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity, 1);
+  Serial.print(",\t");
+  Serial.print(DHT.temperature, 1);
+  Serial.print(",\t");
+  Serial.print(stop - start);
+  Serial.println();
 }
 
 // Request temperature for every DS18B20 device available
@@ -145,57 +173,4 @@ void printAddress(DeviceAddress deviceAddress)
    if (i != 7) Serial.print(":");
  }
  Serial.println();
-}
-
-// Function to request DHT22 status and values
-void requestDHT22Data() {
-  uint32_t start = micros();
-  int chk = DHT.read22(DHT22_PIN);
-  uint32_t stop = micros();
-
-  Serial.print("DHT22,\t");
-  
-//  stat.total++;
-  switch (chk)
-  {
-  case DHTLIB_OK:
-//      stat.ok++;
-      Serial.print("OK,\t");
-      break;
-  case DHTLIB_ERROR_CHECKSUM:
-//      stat.crc_error++;
-      Serial.print("Checksum error,\t");
-      break;
-  case DHTLIB_ERROR_TIMEOUT:
-//      stat.time_out++;
-      Serial.print("Time out error,\t");
-      break;
-  default:
-//      stat.unknown++;
-      Serial.print("Unknown error,\t");
-      break;
-  }
-  // DISPLAY DATA
-  Serial.print(DHT.humidity, 1);
-  Serial.print(",\t");
-  Serial.print(DHT.temperature, 1);
-  Serial.print(",\t");
-  Serial.print(stop - start);
-  Serial.println();
-}
-
-// Method to (re)connect to WiFi network
-void wifiConnection() {
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 }
--- keys.h
+++ keys.h
@@ -1,4 +1,6 @@
-// Project keys
-const char* ssid = "ssid";
-const char* password = "password";
-String apiKey = "thingspeak";
+//----------------  Fill in your credentails   ---------------------
+char ssid[] = "ssid";             // your network SSID (name) 
+char pass[] = "password";         // your network password
+unsigned long myChannelNumber = 000000;  // Replace the 0 with your channel number
+const char * myWriteAPIKey = "0000000000000000";    // Paste your ThingSpeak Write API Key between the quotes 
+//------------------------------------------------------------------
